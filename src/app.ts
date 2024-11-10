100193e47a890b122c9ad175188862b3eede5d13
eldhosechacko <eldhosechacko@gmail.com>
2024-11-10 19:41:47 +0530

    added optionals

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import userRoutes from './routes/userRoutes';
-import aboutRoutes from './routes/aboutRoutes'
+
 
 
 const app = express();
-const port = process.env.PORT || 3001; // Changed to port 3001
+const port = process.env.PORT ?? 3001; // Changed to port 3001
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/demoapi', {
